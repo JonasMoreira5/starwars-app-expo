7e98fbe38805a468e9ee5589b3354f500b40ccc5
Jonas Moreira <jonasmoreira076@gmail.com>
2024-02-11 13:10:18 -0400

    Aula 1

---
+++ src/components/atoms/Text/index.js
@@ -0,0 +1,11 @@
+// Componente generico
+import React from "react"
+import { CustomText } from './styles'
+
+export const Text = ({ children, color}) => {
+    return (
+        <CustomText color={color}>
+            {children}
+        </CustomText>
+    )
+}
